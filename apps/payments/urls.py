c780668f6a3c3841dead239b9041be4b0b740a75
Don-tresor05 <dontresor922@gmail.com>
2026-06-21 12:51:40 +0200

    feat: Register payment status check URL endpoint

--- apps/payments/urls.py
+++ apps/payments/urls.py
@@ -3,5 +3,6 @@ from apps.payments import views
 
 urlpatterns = [
     path("create-checkout-session/", views.create_checkout_session, name="create-checkout-session"),
+    path("check-payment-status/", views.check_payment_status, name="check-payment-status"),
     path("webhook/stripe/", views.stripe_webhook, name="stripe-webhook"),
 ]
